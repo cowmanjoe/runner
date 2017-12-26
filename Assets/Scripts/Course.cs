006b21a961a07814b12185f8f2d8e3b557a855e4
Cowan <ctcumming@gmail.com>
2017-12-26 02:53:53 -0800

    Getting course generation working.

---
+++ Assets/Scripts/Course.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Course
+    {
+        public GameObject platform;
+        public GameObject obstacle;
+
+        public float courseEnd
+        {
+            get { return currentPosition.x;  }
+        } 
+
+        //private float floorLevel;
+        private readonly Vector2 origin;
+        private Vector2 currentPosition;
+        private List<GameObject> terrain;
+        private const float despawnDistance = 50f;
+
+        public Course(Vector2 origin, GameObject platform, GameObject obstacle)
+        {
+            currentPosition = origin;
+            this.platform = platform;
+            this.obstacle = obstacle;
+            terrain = new List<GameObject>();
+        }
+
+        public void Update()
+        {
+            Func<GameObject, bool> outOfRange = go => currentPosition.x - go.transform.position.x > despawnDistance;
+            foreach (var go in terrain.Where(outOfRange)) 
+                UnityEngine.Object.DestroyObject(go);
+            terrain.RemoveAll(go => currentPosition.x - go.transform.position.x > despawnDistance);
+        }
+
+        public void AddGap()
+        {
+            currentPosition += Vector2.right * 2;
+        }
+
+        public void AddFloor()
+        {
+            currentPosition += new Vector2(platform.transform.localScale.x, 0);
+            terrain.Add(UnityEngine.Object.Instantiate(platform, currentPosition, Quaternion.identity));
+        }
+
+        public void AddSmallWall()
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -14,75 +15,95 @@ public class PlatformGenerator : MonoBehaviour
     public float verticalMin = -5f;
     public float verticalMax = 5f;
     public GameObject player;
-    public float despawnDistance = 40f;
+    public float spawnDistance = 5f;
 
-    private Vector2 originPosition;
-    private Queue<GameObject> platforms;
-    private Queue<GameObject> nextObstacles; 
+    //private Vector2 originPosition;
+    //private Queue<GameObject> platforms;
+    //private Queue<GameObject> nextObstacles; 
     private System.Random random;
+    private Course course;
 
     // Use this for initialization
     void Start()
     {
         random = new System.Random(); 
+        course = new Course(platform.transform.position, platform, obstacle);
 
-        originPosition = platform.transform.position;
-        platforms = new Queue<GameObject>();
-        AddNextPlatforms();
+        //originPosition = platform.transform.position;
+        //platforms = new Queue<GameObject>();
     }
 
     void Update()
     {
-        while (platforms.Count < maxPlatforms)
-        {
-            AddNextObstacles(); 
-            AddPlatform();
-        }
+        //while (platforms.Count < maxPlatforms)
+        //{
+        //    AddNextObstacles(); 
+        //    AddPlatform();
+        //}
 
-        var firstPlatform = platforms.Peek();
-        if (player.transform.position.x - firstPlatform.transform.position.x > despawnDistance)
+        //var firstPlatform = platforms.Peek();
+        //if (player.transform.position.x - firstPlatform.transform.position.x > spawnDistance)
+        //{
+        //    Despawn();
+        //}
+
+        if (course.courseEnd - player.transform.position.x < spawnDistance)
         {
-            Despawn();
+            AddCourseSection();
         }
+        course.Update();
     }
 
-    void AddPlatform()
+    void AddCourseSection()
     {
-        var newOrigin = originPosition + new Vector2(platform.transform.localScale.x, 0);
-        platforms.Enqueue(Instantiate(platform, newOrigin, Quaternion.identity));
-        originPosition = newOrigin;
-    }
-
-    // Update is called once per frame
-    void AddNextObstacles()
-    {
-        var num = random.Next(100);
-        Vector2 newOrigin;
-        if (num > 10)
+        var val = random.Next(100);
+        if (val > 90)
         {
-            newOrigin = originPosition + new Vector2(platform.transform.localScale.x, 0);
-            nextPlatforms.Enqueue(Instantiate(platform, newOrigin, Quaternion.identity));
-            originPosition = newOrigin;
+            course.AddGap();
         }
         else
         {
-            var count = 4;
-            newOrigin = originPosition;
-            for (var i = 0; i < count; i++)
-            {
-                newOrigin += new Vector2(platform.transform.localScale.x, 0);
-
-                nextObstacles.Enqueue(Instantiate(obstacle, newOrigin + Vector2.up * 2, Quaternion.identity));
-                nextPlatforms.Enqueue(Instantiate(platform, newOrigin, Quaternion.identity));
-            }
+            course.AddFloor();
         }
     }
 
+    //void AddPlatform()
+    //{
+    //    var newOrigin = originPosition + new Vector2(platform.transform.localScale.x, 0);
+    //    platforms.Enqueue(Instantiate(platform, newOrigin, Quaternion.identity));
+    //    originPosition = newOrigin;
+    //}
+
+    //// Update is called once per frame
+    //void AddNextObstacles()
+    //{
+    //    var num = random.Next(100);
+    //    Vector2 newOrigin;
+    //    if (num > 90)
+    //    {
+    //        newOrigin = originPosition + new Vector2(platform.transform.localScale.x, 0);
+    //        nextObstacles.Enqueue(Instantiate(platform, newOrigin, Quaternion.identity));
+    //        originPosition = newOrigin;
+    //    }
+    //    else
+    //    {
+    //        var count = 4;
+    //        newOrigin = originPosition;
+    //        for (var i = 0; i < count; i++)
+    //        {
+    //            newOrigin += new Vector2(platform.transform.localScale.x, 0);
+
+    //            nextObstacles.Enqueue(Instantiate(obstacle, newOrigin + Vector2.up * 2, Quaternion.identity));
+    //            nextPlatforms.Enqueue(Instantiate(platform, newOrigin, Quaternion.identity));
+    //        }
+    //    }
+    //}
+
     
 
-    void Despawn()
-    {
-        var platform = platforms.Dequeue(); 
-        DestroyObject(platform);
-    }
+    //void Despawn()
+    //{
+    //    var platform = platforms.Dequeue(); 
+    //    DestroyObject(platform);
+    //}
 }
